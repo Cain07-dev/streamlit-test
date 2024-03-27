a5505a18a63e820729af8a81bfd25f7c3dfe0577
Cain07-dev <104369045+Cain07-dev@users.noreply.github.com>
2024-03-27 00:55:35 +0000

    streamlit Barchart

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
-x = st.slider("Select a value")
-st.write(x, "squared is", x * x)
+st.title("Streamlit Test")
+st.header("Barchart")
+
+rand = np.random.normal(1,2, size=20)
+fig, ax = plt.subplots()
+ax.hist(rand, bins=15)
+st.pyplot(fig)
